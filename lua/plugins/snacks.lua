ee7365932e604a2899fe3a07dd48e89b81d1a6e9
nandra <adinandra.dharmasurya@gmail.com>
2026-03-21 00:01:19 +0700

    Enable hidden files

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,18 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    picker = {
+      sources = {
+        files = {
+          hidden = true,
+        },
+        grep = {
+          hidden = true,
+        },
+        explorer = {
+          hidden = true,
+        },
+      },
+    },
+  },
+}
